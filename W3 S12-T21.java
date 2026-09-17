534d703cc1173a24074d326200a73fe27dfb490a
vtu30042-dhanraj <vtu30042@veltech.edu.in>
2026-09-17 09:51:10 +0530

    Create W3 S12-T21.java

---
+++ W3 S12-T21.java
@@ -0,0 +1,19 @@
+#include <stack>
+using namespace std;
+
+class StockSpanner {
+    stack<pair<int, int>> st;
+
+public:
+    int next(int price) {
+        int span = 1;
+
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+
+        st.push({price, span});
+        return span;
+    }
+};
