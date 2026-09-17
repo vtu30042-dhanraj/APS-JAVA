5b87b2ac69a25151c65184e03f25aadf878ca1e9
vtu30042-dhanraj <vtu30042@veltech.edu.in>
2026-09-17 09:34:33 +0530

    Create W3 S11-T20.java

---
+++ W3 S11-T20.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>(); // stores indices
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty()
+                    && temperatures[i] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                answer[prevDay] = i - prevDay;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
